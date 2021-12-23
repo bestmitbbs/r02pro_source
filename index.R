1461fbd713b2a2417b5082ccdbb8930fd1b7234d
r02pro <84392512+r02pro@users.noreply.github.com>
2021-12-22 22:08:19 -0500

    Revert "update"
    This reverts commit 4a1ff6e6d21aeacc305fb09935698943df181ce3.

--- index.R
+++
@@ -1,5 +0,0 @@
-## ---- include=FALSE-------------------------------------------------
-knitr::opts_chunk$set(
-  comment = "#>", echo = TRUE, eval = TRUE, results = FALSE, error = TRUE, fig.width = 6, warning = FALSE
-)
-
